bfdb64be51e1fe8ec0b8bc33e2f7722201ee0479
garynk <geraldshyti@gmail.com>
2018-02-02 18:10:15 +0100

    Added and configured the font inside the Librarian

--- appLibrarian3.0/src/Client/Librarian/LibrarianStyle.java
+++ appLibrarian3.0/src/Client/Librarian/LibrarianStyle.java
@@ -14,7 +14,10 @@ import java.awt.*;
  */
 public class LibrarianStyle {
 
-    private static String FONT_NAME = "Delius";
+    private static String FONT_NAME_PLAIN = "AvenirNext LT Pro Regular";
+    private static String FONT_NAME_BOLD = "AvenirNext LT Pro Bold";
+    private static String FONT_NAME_ITALIC = "AvenirNext LT Pro Cn";
+
 
     public static final java.awt.Color BACKGROUD_DEFAULT_1 = new java.awt.Color(115, 162, 242);
     public static final java.awt.Color BACKGROUD_DEFAULT_2 = new java.awt.Color(150, 200, 255);
@@ -23,17 +26,17 @@ public class LibrarianStyle {
     public static final java.awt.Color SUCCESS_COLOR = new java.awt.Color(0, 153, 0);
     public static final java.awt.Color DEFAULT_BORDER_COLOR = new java.awt.Color(0, 0, 0);
 
-    public static final java.awt.Font BIG_TITLE_FONT = new java.awt.Font(FONT_NAME, Font.ITALIC, 30);
-    public static final java.awt.Font MEDIUM_TITLE_FONT = new java.awt.Font(FONT_NAME, Font.ITALIC, 18);
-    public static final java.awt.Font POINT_CUSTOM_FONT = new java.awt.Font(FONT_NAME, Font.BOLD, 24);
-    public static final java.awt.Font LABEL_FONT = new java.awt.Font(FONT_NAME, Font.BOLD, 15);
-    public static final java.awt.Font LABEL_FONT_2 = new java.awt.Font(FONT_NAME, Font.PLAIN, 12);
-    public static final java.awt.Font LABEL_FONT_3 = new java.awt.Font(FONT_NAME, Font.PLAIN, 13);
-    public static final java.awt.Font DISCORSIVE_LABEL_FONT = new java.awt.Font(FONT_NAME, Font.PLAIN, 13);
-    public static final java.awt.Font OUTPUT_DEFAULT_FONT = new java.awt.Font(FONT_NAME, Font.ITALIC, 11);
-    public static final java.awt.Font BUTTON_FONT = new java.awt.Font(FONT_NAME, Font.BOLD, 11);
-    public static final java.awt.Font EXCEPTION_FONT = new java.awt.Font(FONT_NAME, Font.BOLD, 12);
-    public static final java.awt.Font SUCCESS_LABEL_FONT = new java.awt.Font(FONT_NAME, Font.BOLD, 14);
+    public static final java.awt.Font BIG_TITLE_FONT = new java.awt.Font(FONT_NAME_ITALIC, Font.ITALIC, 37);
+    public static final java.awt.Font MEDIUM_TITLE_FONT = new java.awt.Font(FONT_NAME_ITALIC, Font.ITALIC, 24);
+    public static final java.awt.Font POINT_CUSTOM_FONT = new java.awt.Font(FONT_NAME_BOLD, Font.BOLD, 30);
+    public static final java.awt.Font LABEL_FONT = new java.awt.Font(FONT_NAME_BOLD, Font.BOLD, 15);
+    public static final java.awt.Font LABEL_FONT_2 = new java.awt.Font(FONT_NAME_PLAIN, Font.PLAIN, 12);
+    public static final java.awt.Font LABEL_FONT_3 = new java.awt.Font(FONT_NAME_PLAIN, Font.PLAIN, 13);
+    public static final java.awt.Font DISCORSIVE_LABEL_FONT = new java.awt.Font(FONT_NAME_PLAIN, Font.PLAIN, 13);
+    public static final java.awt.Font OUTPUT_DEFAULT_FONT = new java.awt.Font(FONT_NAME_ITALIC, Font.ITALIC, 11);
+    public static final java.awt.Font BUTTON_FONT = new java.awt.Font(FONT_NAME_BOLD, Font.BOLD, 11);
+    public static final java.awt.Font EXCEPTION_FONT = new java.awt.Font(FONT_NAME_BOLD, Font.BOLD, 12);
+    public static final java.awt.Font SUCCESS_LABEL_FONT = new java.awt.Font(FONT_NAME_BOLD, Font.BOLD, 14);
 
     public static final MatteBorder DEFAULT_MATTE_REGISTRATION = new MatteBorder(0, 0, 1, 0, DEFAULT_BORDER_COLOR);
 
